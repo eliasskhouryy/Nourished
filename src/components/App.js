bb198ac150a79ebfb4fcfd41ddc7b71452841f56
Elias Khoury <eliaskhouryy@hotmail.com>
2022-08-12 11:47:31 +1000

    initial authenticaiton setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-
+import Recipes from "./Recipes";
+import axios from "axios";
+const SERVER_URL = 9201e7486d0055ade0218c1645e9718e —
 function App() {
   return (
     <div className="App">
-    Coming soon
+        <Recipes />
     </div>
   );
 }
-
 export default App;
