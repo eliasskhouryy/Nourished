93a33d3eba4028abefcdd57210db253c8a576c24
Elias Khoury <eliaskhouryy@hotmail.com>
2022-08-17 14:01:36 +1000

    firebase update

--- src/components/Recipes2/AddIngredients.js
+++ src/components/Recipes2/AddIngredients.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import SearchResultShow from './SearchResultShow';
 import '../recipe.css';
+import Profile from '../Authentication/Profile';
+import { useUserAuth } from '../../context/UserAuthContext';
 
 class AddIngredients extends Component {
 	constructor() {
 		super();
 		this.state = {
 			UsersIngredients: [],
-			pantryitems: []
+			pantryitems: [],
 		};
 		this._updateIngredients = this._updateIngredients.bind(this);
 		// this._updatePantry = this._updatePantry.bind(this)
@@ -17,25 +19,25 @@ class AddIngredients extends Component {
 
 	_updateIngredients = (value) => {
 		let updatedIngredients = [];
-		if(this.state.UsersIngredients.includes(value)){
-			updatedIngredients = this.state.UsersIngredients.filter((i)=> i!=value)
+		if (this.state.UsersIngredients.includes(value)) {
+			updatedIngredients = this.state.UsersIngredients.filter((i) => i != value);
 		} else {
-			updatedIngredients = [...this.state.UsersIngredients, value]
+			updatedIngredients = [...this.state.UsersIngredients, value];
 		}
 
 		this.setState({ UsersIngredients: updatedIngredients });
 	};
 
 	// _updatePantry = (value) => {
-    //     this.setState({ pantryitems: [...this.state.pantryitems,value] });
-    // };
+	//     this.setState({ pantryitems: [...this.state.pantryitems,value] });
+	// };
 
 	render() {
 		return (
 			<div className="display_Ingredients">
 				<SearchFormIngredients _updateIngredients={this._updateIngredients} onSubmit={this._updateIngredients} ingredients={this.state.UsersIngredients} onClick={this._handleClick} />
 				<DisplayIngredients UsersIngredients={this.state.UsersIngredients} />
-				<PantryForm onChange={this._updateIngredients} UsersIngredients={this.state.UsersIngredients}  />
+				<PantryForm onChange={this._updateIngredients} UsersIngredients={this.state.UsersIngredients} />
 			</div>
 		);
 	}
@@ -53,6 +55,8 @@ const DisplayIngredients = (props) => {
 };
 
 const SearchFormIngredients = (props) => {
+	const { logOut, user } = useUserAuth();
+
 	const [value, setValue] = useState('');
 	const _handleSubmit = (event) => {
 		event.preventDefault();
@@ -72,42 +76,41 @@ const SearchFormIngredients = (props) => {
 			</Link>
 		</div>
 	);
-
 };
 
 const PantryForm = (props) => {
-    console.log(props)
-    const [garlic,setFirst]=useState(true);
-    const [oliveoil,setSecond]=useState(true);
-    const handleChange = (data) => {
-            //     if(garlic === true){
-            //     console.log(data)
-            //     props.onChange(data)
-            // }
-		if ( data === 'garlic'){
-			setFirst(!garlic) 
+	console.log(props);
+	const [garlic, setFirst] = useState(true);
+	const [oliveoil, setSecond] = useState(true);
+	const handleChange = (data) => {
+		//     if(garlic === true){
+		//     console.log(data)
+		//     props.onChange(data)
+		// }
+		if (data === 'garlic') {
+			setFirst(!garlic);
 		}
 
 		// if(oliveoil === true ){
 		// 	console.log(data)
 		// 	props.onChange(data)
 		// }
-		
-		if ( data === 'oliveoil'){
-			setSecond(!oliveoil)
-		}
-		props.onChange(data)
-        }
 
-    return(
-        <div className = "mainSearch pantryform">
-            Your Pantry
-            <br></br>
-            <input onChange = {()=>handleChange("garlic")} type = "checkbox"  value = {garlic} /> Garlic
-            <input onChange = {()=>handleChange("oliveoil")} type = "checkbox"  value = {oliveoil}  /> Olive Oil
-            {/* <p>Pantryitems: {props.UsersIngredients.length}</p> */}
-        </div>
-    )
-}
+		if (data === 'oliveoil') {
+			setSecond(!oliveoil);
+		}
+		props.onChange(data);
+	};
+
+	return (
+		<div className="mainSearch pantryform">
+			Your Pantry
+			<br></br>
+			<input onChange={() => handleChange('garlic')} type="checkbox" value={garlic} /> Garlic
+			<input onChange={() => handleChange('oliveoil')} type="checkbox" value={oliveoil} /> Olive Oil
+			{/* <p>Pantryitems: {props.UsersIngredients.length}</p> */}
+		</div>
+	);
+};
 
 export default AddIngredients;
